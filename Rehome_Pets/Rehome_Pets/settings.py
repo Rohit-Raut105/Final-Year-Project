b8b0d283db66f9dbebb361cd354ccee9449dcb51
Rohit-Raut105 <anupraut105@gmail.com>
2025-01-07 13:25:58 +0545

    Final changes to login

--- Rehome_Pets/Rehome_Pets/settings.py
+++ Rehome_Pets/Rehome_Pets/settings.py
@@ -122,8 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Use SMTP backend
 EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
 EMAIL_PORT = 587  # Use port 587 for TLS
